13d848b4ae07fbe9f6e63aab2725ec5dfc09b2cb
Emily Baltz <emily.baltz@gmail.com>
2018-02-21 10:56:29 -0800

    Add files via upload

--- MEDPC_behavioral_analyses.m
+++ MEDPC_behavioral_analyses.m
@@ -1,12 +1,12 @@
 %% MEDPC data for timing of events
-% INTRODUCTORY TEXT
-%The goal of this program is to take raw MEDPC data and produce usable
-%information on the timing of each event type (licks, lever presses, head
-%entries, reinforcements) and the intervals between events. This includes a
-%number of analyses of the microstructure of licking behavior (see
-%below). There is optional code that is commented out for more variables or
-%figures. This was last updated 1/9/18 by Emily Baltz. (Gremel Lab, UCSD
-%Dept of Psychology. Support: Please contact cgremel at ucsd.edu)
+INTRODUCTORY TEXT
+The goal of this program is to take raw MEDPC data and produce usable
+information on the timing of each event type (licks, lever presses, head
+entries, reinforcements) and the intervals between events. This includes a
+number of analyses of the microstructure of licking behavior (see
+below). There is optional code that is commented out for more variables or
+figures. This was last updated 1/9/18 by Emily Baltz. (Gremel Lab, UCSD
+Dept of Psychology. Support: Please contact cgremel at ucsd.edu)
 
 % FEATURES: 
 % ?	Graph of cumulative licks/time
